e297198ed4059148785bf52fdedc0dbbe7c3643f
danklul <daniel_ekroth@hotmail.com>
2019-11-25 02:09:37 +0100

    Final commit

---
+++ Position.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace Labb2
+{
+    public class Position
+    {
+        public int xPos;
+        public int yPos;
+
+        public Position(int x, int y)
+        {
+            this.xPos = Math.Abs(x);
+            this.yPos = Math.Abs(y);
+        }
+
+        public double Length()
+        {
+            var c = Math.Sqrt(xPos * xPos + yPos * yPos);
+            return c;
+        }
+
+        public bool Equals(Position p)
+        {
+            if (p.xPos == xPos && p.yPos == yPos)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Position Clone()
+        {
+            Position newPosition = new Position(x: xPos, y: yPos);
+            return newPosition;
+        }
+
+        public override string ToString()
+        {
+            return $"{xPos} : {yPos}";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Length() > p2.Length())
+            {
+                return true;
+            }
+            if (p1.Length() >= p2.Length())
+            {
+                if (p1.xPos > p2.xPos)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+
+            return true;
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            var newX = p1.xPos + p2.xPos;
+            var newY = p1.yPos + p2.yPos;
+            return new Position(newX, newY);
+        }
+        public static Position operator -(Position p1, Position p2)
+        {
+            var newX = p1.xPos - p2.xPos;
+            var newY = p1.yPos - p2.yPos;
+            return new Position(newX, newY);
+        }
+        public static double operator %(Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.xPos - p2.xPos, 2) + Math.Pow(p1.yPos - p2.yPos, 2));
+        }
+    }
+}
---
+++ SortedPosList.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Labb2
+{
+    public class SortedPosList
+    {
+        public List<Position> positionList = new List<Position>();
+        private Position position;
+        public SortedPosList()
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", positionList);
+        }
+
+        public int Count()
+        {
+            return positionList.Count;
+        }
+
+        public void Add(Position pos)
+        {
+            positionList.Add(pos);
+            positionList.Sort((x, y) => x.Length().CompareTo(y.Length()));
+        }
+
+        public bool Remove(Position pos)
+        {
+            for (int i = 0; i < positionList.Count; i++)
+            {
+                if (pos.xPos == positionList[i].xPos && pos.yPos == positionList[i].yPos)
+                {
+                    positionList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList clonedList = new SortedPosList();
+            foreach (var pos in positionList)
+            {
+                var newpos = pos.Clone();
+                clonedList.Add(newpos);
+            }
+            return clonedList;
+        }
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList newCircleList = Clone();
+            foreach (var pos in positionList)
+            {
+                if (Math.Pow(pos.xPos - centerPos.xPos, 2) + Math.Pow(pos.yPos - centerPos.yPos, 2) > Math.Pow(radius, 2))
+                {
+                    newCircleList.Remove(pos);
+                }
+            }
+            return newCircleList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+
+            for (int i = 0; i < sp2.Count(); i++)
+            {
+                sp1.Add(sp2[i]);
+            }
+            return sp1;
+        }
+
+        public static SortedPosList operator -(SortedPosList a, SortedPosList b)
+        {
+            var list = a.Clone();
+
+            for (int i = 0; i < a.Count(); i++)
+            {
+                for (int y = 0; y < b.Count(); y++)
+                {
+                    if (a[i].xPos == b[y].xPos && a[i].yPos == b[y].yPos)
+                    {
+                        list.Remove(a[i]);
+                    }
+                }
+            }
+            return list;
+        }
+
+        public Position this[int i]
+        {
+            get { return positionList[i]; }
+        }
+    }
+}
